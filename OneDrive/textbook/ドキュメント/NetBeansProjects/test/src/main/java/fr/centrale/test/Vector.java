da90a6aa2a3665dd3466e394b0d81756fbe3b71d
Tatsuya0924 <wootatsuya0924@gmail.com>
2025-01-09 12:08:12 +0100

    test case

---
+++ OneDrive/textbook/ドキュメント/NetBeansProjects/test/src/main/java/fr/centrale/test/Vector.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.centrale.test;
+
+/**
+ *
+ * @author woota
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public final class Vector {
+
+    private Vector() {}
+
+    /**
+     * Checks whether the given vectors are equal.
+     * @param a
+     * @param b
+     * @return 
+     */
+    public static boolean equal(int[] a, int[] b) {
+        if ((a == null) || (b == null)) {
+            throw new IllegalArgumentException("null argument");
+        }
+
+        if (a.length != b.length) {
+            return false;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Scalar multiplication of given vectors.
+     * @param a
+     * @param b
+     * @return 
+     */
+    public static int scalarMultiplication(int[] a, int[] b) {
+        if ((a == null) || (b == null)) {
+            throw new IllegalArgumentException("null argument");
+        }
+
+        if (a.length != b.length) {
+            throw new IllegalArgumentException(
+                    "different tuple of the vectors ("
+                    + a.length + ", " + b.length + ')');
+        }
+
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+        }
+        return sum;
+    }
+
+}
